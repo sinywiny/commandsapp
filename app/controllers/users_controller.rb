b4ede4fa13c3cf9f46bf5f64cdfc60407b10bd23
Sinead Fernandez <sineadd.fernandez@gmail.com>
2023-03-09 12:46:14 +0100

    Add edit template and update action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new 
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was succesfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            flash[:notice] = "Welcome to CommandsApp #{@user.username}, you have succesfully signed up"
+            redirect_to articles_path 
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+end
